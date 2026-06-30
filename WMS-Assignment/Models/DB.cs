b12b31b0ca3ff305cad477e18092ab82e9934014
sjwong07 <shengjiewong97@gmail.com>
2026-06-30 14:25:29 +0800

    database

--- WMS-Assignment/Models/DB.cs
+++ WMS-Assignment/Models/DB.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
+namespace WMS_Assignment.Models;
+#nullable disable warnings
 public class DB(DbContextOptions options) : DbContext(options)
 {
 
@@ -10,113 +12,115 @@ public class DB(DbContextOptions options) : DbContext(options)
     public DbSet<Category> Categories { get; set; }
     public DbSet<MenuItem> MenuItems { get; set; }
 
-
-    public class Role
-    {
-        [Key, MaxLength(10)]
-        public string Id { get; set; }
-
-        [MaxLength(100)]
-        public string Name { get; set; }
-
-        [MaxLength(100)]
-        public string Description { get; set; }
-
-        public List<Role> Roles { get; set; } = [];
-    }
-    public class User
-    {
-        [Key, MaxLength(100)]
-        public string Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string Name {  get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string Email { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string Password { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string FirstName { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string LastName { get; set; }
+}
 
 
-        [Required]
-        public DateTime CreatedDate { get; set; }
+public class Role
+{
+    [Key, MaxLength(10)]
+    public string Id { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public string RoleId { get; set; }
-        public Role Role { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
 
-        public List<User> Users { get; set; } = [];
+    [MaxLength(100)]
+    public string Description { get; set; }
 
-    }
-    public class Category
-    {
-        [Key, MaxLength(10)]
-        public string Id { get; set; }
+    public List<Role> Roles { get; set; } = [];
+}
+public class User
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
 
-        [MaxLength(100)]
-        public string Name { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Name {  get; set; }
 
-        [MaxLength(100)]
-        public string Description { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Email { get; set; }
 
-        public List<Category> Categories { get; set; } = [];
+    [Required]
+    [MaxLength(100)]
+    public string Password { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; }
+
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string RoleId { get; set; }
+    public Role Role { get; set; }
+
+    public List<User> Users { get; set; } = [];
+
+}
+public class Category
+{
+    [Key, MaxLength(10)]
+    public string Id { get; set; }
+
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MaxLength(100)]
+    public string Description { get; set; }
+
+    public List<Category> Categories { get; set; } = [];
 
         
-    }
-
-    public class MenuItem
-    {
-        [Key, MaxLength(100)]
-        public string Id { get; set; }
-
-        [Required]
-        [MaxLength(50)]
-        public string Name { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        public string Description { get; set; }
-
-        [Required]
-        public decimal Price { get; set; }
-
-        [MaxLength(50)]
-        public string CategoryId { get; set; }
-        public Category Category { get; set; }
-
-        public List<MenuItem> MenuItems { get; set; } = [];
-    }
-
-    public class Table
-    {
-        [Key, MaxLength(100)]
-        public string Id { get; set; }
-
-        [Required]
-        [MaxLength(10)]
-        public string Number { get; set; }
-
-        [Required]
-        [MaxLength(10)]
-        public int Capacity { get; set; }
-
-        [Required]
-        [MaxLength(30)]
-        public string Location { get; set; }
-
-    }
 }
+
+public class MenuItem
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Description { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    [MaxLength(50)]
+    public string CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    public List<MenuItem> MenuItems { get; set; } = [];
+}
+
+public class Table
+{
+    [Key, MaxLength(100)]
+    public string Id { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    public string Number { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    public int Capacity { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    public string Location { get; set; }
+
+}
+
     
--- WMS-Assignment/Program.cs
+++ WMS-Assignment/Program.cs
@@ -1,18 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using WMS_Assignment.Data;
-using WMS_Assignment.Services;
+global using WMS_Assignment.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<DB>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlOptions => sqlOptions.MigrationsAssembly(typeof(DB).Assembly.FullName)));
-
-
-builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddSqlServer<DB>($@"
+    Data Source=(LocalDB)\MSSQLLocalDB;
+    AttachDbFilename={builder.Environment.ContentRootPath}\DB.mdf;
+");
 
 // Configure Authentication with Cookie
 builder.Services.AddAuthentication(options =>
@@ -33,45 +30,13 @@ builder.Services.AddAuthentication(options =>
     options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
 });
 
+
+
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-else
-{
-    app.UseDeveloperExceptionPage();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Seed database
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<DB>();
-        context.Database.EnsureCreated();
-        // SeedData.Initialize(services);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
-}
+app.MapDefaultControllerRoute();
+ 
 
 app.Run();
