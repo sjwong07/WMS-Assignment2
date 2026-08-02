36ef442c473a536bc28a344519cde7467dc639a4
TobyNgoh <youxianngoh26@gmail.com>
2026-08-02 14:17:05 +0800

    Fix login lockout logic, add Logout, fix missing _ViewImports.cshtml (was breaking asp- tag helpers site-wide)

---
+++ WMS-Assignment/Views/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WMS_Assignment.Views
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
