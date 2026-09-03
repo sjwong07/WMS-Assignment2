09544917c107e5cd9c8405e36b7bf6006117bd5f
ongsirhoong931 <ongsirhoong931@gmail.com>
2026-09-03 23:33:05 +0800

    edit top selling product

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿UPDATE m
+SET m.PhotoURL = p.PhotoURL
+FROM MenuItems m
+INNER JOIN (
+    SELECT MenuItemId, PhotoURL, ROW_NUMBER() OVER (PARTITION BY MenuItemId ORDER BY Id) as rn
+    FROM MenuItemPhotos
+) p ON m.Id = p.MenuItemId
+WHERE p.rn = 1 AND (m.PhotoURL IS NULL OR m.PhotoURL = '');
